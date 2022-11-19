f574024fa89939e1d9f74b91fb4b865edfd5a6d2
Christophe Simon <csimon.developpement@gmail.com>
2022-11-19 14:14:59 +0000

    Prevent document uploaded by employee from being something else that a jpg, jpeg or png file

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,28 +17,35 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     e.preventDefault()
+    document.querySelector(".error-extension-file").style.display = "none"
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+    const fileExtension = fileName.split('.').pop()
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
     formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    if(['jpg','jpeg','png'].includes(fileExtension)) {
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({fileUrl, key}) => {
+          console.log(fileUrl)
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    } else {
+      document.querySelector(".error-extension-file").style.display = "block"
+      // document.querySelector(`input[data-testid="file"]`).value = null
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
